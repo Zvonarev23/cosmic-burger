b4f419c0f7ab2bb261a01954e95b40ecbd2875e6
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2023-03-11 18:04:46 +0300

    moved the ingredient filtering logic before jsx and used useMemo

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,5 +1,5 @@
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import styles from "./burger-ingredients.module.css";
 import { IngrediensGroup } from "./ingredients-group/ingredients-group.jsx";
 import PropTypes from "prop-types";
@@ -7,9 +7,17 @@ import PropTypes from "prop-types";
 export const BurgerIngredients = (props) => {
   const [current, setCurrent] = useState("Булки");
 
-  const filterIngredientsListByType = (arr, type) => {
-    return arr.filter((item) => item.type === type);
-  };
+  const buns = useMemo(() => {
+    return props.ingredientsList.filter((item) => item.type === "bun");
+  }, [props.ingredientsList]);
+
+  const sauces = useMemo(() => {
+    return props.ingredientsList.filter((item) => item.type === "sauce");
+  }, [props.ingredientsList]);
+
+  const mains = useMemo(() => {
+    return props.ingredientsList.filter((item) => item.type === "main");
+  }, [props.ingredientsList]);
 
   return (
     <div className="pt-10">
@@ -34,29 +42,11 @@ export const BurgerIngredients = (props) => {
       </div>
 
       <div className={`${styles.wrapper} custom-scroll`}>
-        <IngrediensGroup
-          type="Булки"
-          ingredientsType={filterIngredientsListByType(
-            props.ingredientsList,
-            "bun"
-          )}
-        />
+        <IngrediensGroup type="Булки" ingredientsType={buns} />
 
-        <IngrediensGroup
-          type="Соусы"
-          ingredientsType={filterIngredientsListByType(
-            props.ingredientsList,
-            "sauce"
-          )}
-        />
+        <IngrediensGroup type="Соусы" ingredientsType={sauces} />
 
-        <IngrediensGroup
-          type="Начинки"
-          ingredientsType={filterIngredientsListByType(
-            props.ingredientsList,
-            "main"
-          )}
-        />
+        <IngrediensGroup type="Начинки" ingredientsType={mains} />
       </div>
     </div>
   );
