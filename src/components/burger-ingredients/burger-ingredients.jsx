91b2592bb256198072bb4b5b7e80eefbc0266018
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2023-03-18 00:03:05 +0300

    added smooth scroll in burger-ingredients component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,6 +1,6 @@
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { commonPropTypes } from "../../utils/commonPropTypes.js";
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import styles from "./burger-ingredients.module.css";
 import { IngrediensGroup } from "./ingredients-group/ingredients-group.jsx";
 import PropTypes from "prop-types";
@@ -8,11 +8,31 @@ import { Modal } from "../modal/modal.jsx";
 import { IngredientDetails } from "./ingredient-details/ingredient-details.jsx";
 
 export const BurgerIngredients = ({ ingredientsList }) => {
-  const [current, setCurrent] = useState("Булки");
+  const [current, setCurrent] = useState("bun");
   const [ingredientDetails, setIngredientDetails] = useState({
     currentIngredient: null,
     isOpenIngredientDetails: false,
   });
+
+  const tabBunRef = useRef(null);
+  const tabSauceRef = useRef(null);
+  const tabMainRef = useRef(null);
+
+  const selectCategory = (category) => {
+    setCurrent(category);
+
+    switch (category) {
+      case "bun":
+        tabBunRef.current.scrollIntoView({ behavior: "smooth" });
+        break;
+      case "sauce":
+        tabSauceRef.current.scrollIntoView({ behavior: "smooth" });
+        break;
+      case "main":
+        tabMainRef.current.scrollIntoView({ behavior: "smooth" });
+        break;
+    }
+  };
 
   const openIngredientDetails = (item) => {
     setIngredientDetails({
@@ -43,43 +63,50 @@ export const BurgerIngredients = ({ ingredientsList }) => {
   return (
     <div className="pt-10">
       <h1 className={`text text_type_main-large mb-5`}>Соберите бургер</h1>
-
       <div className={`${styles.tabs} mb-10`}>
-        <Tab value="Булки" active={current === "Булки"} onClick={setCurrent}>
+        <Tab value="bun" active={current === "bun"} onClick={selectCategory}>
           Булки
         </Tab>
 
-        <Tab value="Соусы" active={current === "Соусы"} onClick={setCurrent}>
+        <Tab
+          value="sauce"
+          active={current === "sauce"}
+          onClick={selectCategory}
+        >
           Соусы
         </Tab>
 
-        <Tab
-          value="Начинки"
-          active={current === "Начинки"}
-          onClick={setCurrent}
-        >
+        <Tab value="main" active={current === "main"} onClick={selectCategory}>
           Начинки
         </Tab>
       </div>
 
       <div className={`${styles.wrapper} custom-scroll`}>
-        <IngrediensGroup
-          type="Булки"
-          openIngredientDetails={openIngredientDetails}
-          ingredientsType={buns}
-        />
+        <ul>
+          <li ref={tabBunRef}>
+            <IngrediensGroup
+              type="Булки"
+              openIngredientDetails={openIngredientDetails}
+              ingredientsType={buns}
+            />
+          </li>
 
-        <IngrediensGroup
-          type="Соусы"
-          openIngredientDetails={openIngredientDetails}
-          ingredientsType={sauces}
-        />
+          <li ref={tabSauceRef}>
+            <IngrediensGroup
+              type="Соусы"
+              openIngredientDetails={openIngredientDetails}
+              ingredientsType={sauces}
+            />
+          </li>
 
-        <IngrediensGroup
-          type="Начинки"
-          openIngredientDetails={openIngredientDetails}
-          ingredientsType={mains}
-        />
+          <li ref={tabMainRef}>
+            <IngrediensGroup
+              type="Начинки"
+              openIngredientDetails={openIngredientDetails}
+              ingredientsType={mains}
+            />
+          </li>
+        </ul>
       </div>
       {ingredientDetails.isOpenIngredientDetails && (
         <Modal onClose={closeIngredientDetails}>
