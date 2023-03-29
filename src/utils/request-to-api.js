6f395ba57653779183c5d9e8741474a22373d901
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2023-03-29 15:51:54 +0300

    removed code duplication in request

--- src/utils/request-to-api.js
+++ src/utils/request-to-api.js
@@ -1,27 +1,24 @@
 const API_URL = "https://norma.nomoreparties.space/api";
 
-export const getIngredients = () => {
-  return fetch(`${API_URL}/ingredients`).then((response) => {
-    if (!response.ok) {
-      throw new Error("Что-то пошло не так... Статус: " + response.status);
-    } else {
-      return response.json();
+const request = (endpoint, options) => {
+  return fetch(`${API_URL}${endpoint}`, options).then((res) => {
+    if (res.ok) {
+      return res.json();
     }
+    throw new Error(res.status);
   });
 };
 
+export const getIngredients = () => {
+  return request("/ingredients");
+};
+
 export const sendOrder = (order) => {
-  return fetch(`${API_URL}/orders`, {
+  return request("/order", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(order),
-  }).then((res) => {
-    if (!res.ok) {
-      return Promise.reject(res.status);
-    } else {
-      return res.json();
-    }
   });
 };
