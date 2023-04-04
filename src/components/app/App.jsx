c91d48cd1ff25c8fe4b7d3004dfa6e4d85a97051
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2023-04-04 13:51:48 +0300

    added DndProvider in project

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,14 +2,18 @@ import { AppHeader } from "../app-header/app-header.jsx";
 import { BurgerIngredients } from "../burger-ingredients/burger-ingredients.jsx";
 import { BurgerConstructor } from "../burger-constructor/burger-constructor.jsx";
 import { Main } from "../main/main.jsx";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
       <Main>
-        <BurgerIngredients />
-        <BurgerConstructor />
+        <DndProvider backend={HTML5Backend}>
+          <BurgerIngredients />
+          <BurgerConstructor />
+        </DndProvider>
       </Main>
     </div>
   );
